6e060ff9dc8c3e70111ba31247888b1713de2f24
AmritPrajapat <amritlalprajapat93@gmail.com>
2020-04-15 14:53:11 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counterExample';
+  countValue : number=0;
+  IncreaseCounter()
+  {
+    this.countValue++;
+  }
+
+  CleasrCounter()
+  {
+    this.countValue = 0;
+  }
+
+
 }
